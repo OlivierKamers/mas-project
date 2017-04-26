ff189d6951f75be86245062098725be74ecf1d77
Olivier Kamers <olivier.kamers@hotmail.com>
2017-04-26 12:24:27 +0200

    Afronden

--- scripts/clean_taxi_data.R
+++ scripts/clean_taxi_data.R
@@ -7,5 +7,7 @@ MAX_LATI = 40.8774528503
 load_data <- function(filename) {
   d <- fread(filename, header = TRUE, sep = ',', select = c('tpep_pickup_datetime','tpep_dropoff_datetime','passenger_count','pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude'))
   d_filtered <- d[d$pickup_longitude %between% c(MIN_LONG,MAX_LONG) & d$dropoff_longitude %between% c(MIN_LONG,MAX_LONG) & d$pickup_latitude %between% c(MIN_LATI, MAX_LATI) & d$dropoff_latitude %between% c(MIN_LATI, MAX_LATI) , ]
+  cols <- c('pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude')
+  d_filtered[,(cols) := round(.SD,6), .SDcols=cols]
   write.csv(d_filtered, gsub(".csv", "_cleaned.csv", filename), row.names=FALSE)
 }
