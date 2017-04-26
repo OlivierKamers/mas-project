1335444188e539e61cf385695facd50b9501b4df
Olivier Kamers <olivier.kamers@hotmail.com>
2017-04-26 22:52:05 +0200

    Sort data by pickup datetime to be able to read csv sequentially in Java. Begin work on a DataLoader (just trying out)

--- scripts/clean_taxi_data.R
+++ scripts/clean_taxi_data.R
@@ -5,9 +5,18 @@ MAX_LONG = -73.9104537964
 MIN_LATI = 40.7011375427
 MAX_LATI = 40.8774528503
 load_data <- function(filename) {
-  d <- fread(filename, header = TRUE, sep = ',', select = c('tpep_pickup_datetime','tpep_dropoff_datetime','passenger_count','pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude'))
+  d <- fread(filename,
+             header = TRUE,
+             sep = ',',
+             select = c('tpep_pickup_datetime','tpep_dropoff_datetime','passenger_count','pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude')
+             )
+  print('Filtering coordinates')
   d_filtered <- d[d$pickup_longitude %between% c(MIN_LONG,MAX_LONG) & d$dropoff_longitude %between% c(MIN_LONG,MAX_LONG) & d$pickup_latitude %between% c(MIN_LATI, MAX_LATI) & d$dropoff_latitude %between% c(MIN_LATI, MAX_LATI) , ]
+  print('Rounding coordinates')
   cols <- c('pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude')
   d_filtered[,(cols) := round(.SD,6), .SDcols=cols]
+  print('Sorting by pickup datetime')
+  d_filtered <- d_filtered[order(as.POSIXct(d_filtered$tpep_pickup_datetime, format="%Y-%m-%d %H:%M:%S")),]
+  print('Writing csv')
   write.csv(d_filtered, gsub(".csv", "_cleaned.csv", filename), row.names=FALSE)
 }
---
+++ src/main/java/HistoricalData.java
@@ -0,0 +1,90 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class HistoricalData {
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private LocalDateTime pickupTime;
+    private LocalDateTime dropoffTime;
+    private Number passengerCount;
+    private Number pickupLongitude;
+    private Number pickupLatitude;
+    private Number dropoffLongitude;
+    private Number dropoffLatitude;
+
+    public HistoricalData(LocalDateTime pickupTime, LocalDateTime dropoffTime, Number passengerCount, Number pickupLongitude, Number pickupLatitude, Number dropoffLongitude, Number dropoffLatitude) {
+        this.pickupTime = pickupTime;
+        this.dropoffTime = dropoffTime;
+        this.passengerCount = passengerCount;
+        this.pickupLongitude = pickupLongitude;
+        this.pickupLatitude = pickupLatitude;
+        this.dropoffLongitude = dropoffLongitude;
+        this.dropoffLatitude = dropoffLatitude;
+    }
+
+    public static HistoricalData parse(String pickupTime, String dropoffTime, String passengerCount, String pickupLongitude, String pickupLatitude, String dropoffLongitude, String dropoffLatitude) throws ParseException {
+        return new HistoricalData(
+                LocalDateTime.parse(pickupTime.replaceAll("\"", ""), dateTimeFormatter),
+                LocalDateTime.parse(dropoffTime.replaceAll("\"", ""), dateTimeFormatter),
+                NumberFormat.getNumberInstance(Locale.US).parse(passengerCount),
+                NumberFormat.getNumberInstance(Locale.US).parse(pickupLongitude),
+                NumberFormat.getNumberInstance(Locale.US).parse(pickupLatitude),
+                NumberFormat.getNumberInstance(Locale.US).parse(dropoffLongitude),
+                NumberFormat.getNumberInstance(Locale.US).parse(dropoffLatitude)
+        );
+    }
+
+    public static HistoricalData parse(String[] fields) throws ParseException {
+        return parse(fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], fields[6]);
+    }
+
+    public LocalDateTime getPickupTime() {
+        return pickupTime;
+    }
+
+    public LocalDateTime getDropoffTime() {
+        return dropoffTime;
+    }
+
+    public Number getPickupLongitude() {
+        return pickupLongitude;
+    }
+
+    public Number getPickupLatitude() {
+        return pickupLatitude;
+    }
+
+    public Number getDropoffLongitude() {
+        return dropoffLongitude;
+    }
+
+    public Number getDropoffLatitude() {
+        return dropoffLatitude;
+    }
+
+    public Number getPassengerCount() {
+        return passengerCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append('[')
+                .append(getPickupTime().toString())
+                .append("] ")
+                .append(getPassengerCount().toString())
+                .append(" passenger(s) (")
+                .append(getPickupLongitude().toString())
+                .append(", ")
+                .append(getPickupLatitude().toString())
+                .append(") --> (")
+                .append(getDropoffLongitude().toString())
+                .append(", ")
+                .append(getDropoffLatitude().toString())
+                .append(')')
+        ;
+        return sb.toString();
+    }
+}
