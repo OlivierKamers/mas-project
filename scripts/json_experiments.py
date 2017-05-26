0d5bcb043962a2ddb3dfca7f27413d992acaefe7
Olivier Kamers <olivier.kamers@hotmail.com>
2017-05-26 10:54:08 +0200

    Sampling

--- scripts/json_experiments.py
+++ scripts/json_experiments.py
@@ -1,7 +1,9 @@
 import fnmatch
 import json
 import os
+from math import ceil
 
+import numpy as np
 from matplotlib import pyplot as plt
 
 path = os.path.join('..', 'stats')
@@ -21,18 +23,33 @@ def make_plots(timestamp, j):
   make_double_plot(timestamp, j, 'Amount of idle taxis and waiting customers', 'amountOfIdleTaxis', 'amountOfWaitingCustomers')
 
 
+def average(arr, n):
+  end = n * int(len(arr) / n)
+  return np.mean(np.asarray(arr)[:end].reshape(-1, n), 1)
+
+
 def make_double_plot(timestamp, j, title, k1, k2, xlabel='Tick'):
   fig, ax1 = plt.subplots()
 
-  ax1.plot(j[k1], 'r-')
+  points = 500
+  x_max = max(len(j[k1]), len(j[k2]))
+  sample = int(ceil(1.0 * x_max / points))
+
+  y1 = average(j[k1], sample)
+  x1 = np.linspace(0, x_max, num=len(y1))
+  ax1.plot(x1, y1, 'r-')
   ax1.set_xlabel(xlabel)
   ax1.set_ylabel(k1, color='r')
   ax1.tick_params('y', colors='r')
 
   ax2 = ax1.twinx()
-  ax2.plot(j[k2], 'b-')
+  y2 = average(j[k2], sample)
+  x2 = np.linspace(0, x_max, num=len(y2))
+  ax2.plot(x2, y2, 'b-')
   ax2.set_ylabel(k2, color='b')
   ax2.tick_params('y', colors='b')
+
+  plt.ylim(ymin=0)
 
   plt.title(title)
   fig.tight_layout()
